27457ed15b6dcbe00f2736f53976924f0b6772b9
amithah <amithah.nithin@gmail.com>
2020-06-28 11:43:41 +0400

    third commit

--- demo/settings.py
+++ demo/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 #-----------------------------------------heroku-----------------   
 # Extra lookup directories for collectstatic to find static files
